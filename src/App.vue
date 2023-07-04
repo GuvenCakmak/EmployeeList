ebb9b161aaade73772132da909decfb2e9ac087c
Güven <guven_cakmak@hotmail.com>
2023-07-04 21:34:42 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  
+  <div class="justify-center flex bg-yellow-700 items-center">
+<h2>test</h2>
+<div class="text-4lg">
+
+  Hello 👋🏼
+
+</div>
+
+</div>
+</template>
+
+<script>
+
+import "./input.css"
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+ 
+}
+</script>
+
+<style>
+
+</style>
